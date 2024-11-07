035fa6d599e395f89bd687746092e0ffb7ec1e6c
Eric Anderson <e@ericlanderson.com>
2024-11-07 18:13:14 +0000

    More output

--- index.js
+++ index.js
@@ -15,8 +15,18 @@ function mk(a) {
 const gt = mk('gt');
 const satisfies = mk('satisfies');
 
-gt("1.2.0-beta.3", "1.2.0-rc.1");
-satisfies("1.2.0-beta.3", "^1.2.0-rc.1");
-satisfies("1.2.0-beta.3", "^1.2.0-beta.4");
-satisfies("1.2.0-beta.3", "^1.2.0-beta.3");
-satisfies("1.2.0-rc.3", "^1.2.0-beta.3");
+gt("1.2.0-beta.3", "1.2.0-rc.1"); // = false
+
+// beta does not satisfy rc
+satisfies("1.2.0-beta.3", "^1.2.0-rc.1"); // = false
+
+satisfies("1.2.0-beta.3", "^1.2.0-beta.4"); // = false
+
+
+satisfies("1.2.0-beta.3", "^1.2.0-beta.3"); // = true
+
+// rc satisifes beta cause its higher
+satisfies("1.2.0-rc.3", "^1.2.0-beta.3"); // = true
+
+// enforces ^ stays within pre-release versions
+satisfies("1.3.0-beta.3", "^1.2.0-beta.3"); // = false
